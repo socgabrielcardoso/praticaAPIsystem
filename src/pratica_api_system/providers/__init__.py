ff4cb8a1f023794e3dcb1bb5720d9699059318f6
Gabriel de Lima Cardoso <gabriellimacardoso9@gmail.com>
2026-09-21 00:11:57 -0300

    Expose all enrichment providers

--- src/pratica_api_system/providers/__init__.py
+++ src/pratica_api_system/providers/__init__.py
@@ -1,11 +1,17 @@
 from .abuseipdb import AbuseIPDBProvider
+from .epss import EPSSProvider
+from .greynoise import GreyNoiseProvider
 from .nvd import NVDProvider
+from .otx import OTXProvider
 from .urlhaus import URLhausProvider
 from .virustotal import VirusTotalProvider
 
 __all__ = [
     "AbuseIPDBProvider",
+    "EPSSProvider",
+    "GreyNoiseProvider",
     "NVDProvider",
+    "OTXProvider",
     "URLhausProvider",
     "VirusTotalProvider",
 ]
